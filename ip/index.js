ad040375804ad6945bb605b00ed77b201fb34a85
Eray Yıldız <eray1yildiz@hotmail.com>
2023-05-26 00:00:50 +0300

    tamamlandı

--- ip/index.js
+++ ip/index.js
@@ -1,26 +1,24 @@
 //axios import buraya gelecek
-
+import axios from "axios";
 var benimIP;
-
 
 // ------------ değiştirmeyin --------------
 // licensed to Ergineer 2022
 require("babel-core/register");
 require("babel-polyfill");
-async function ipAdresimiAl(){
-	await axios({
-		method: 'get',
-		url: 'https://apis.ergineer.com/ipadresim',
-	})
-	.then(function (response) {
-		return response.data
-	})
-	.then(function (a) {
-		benimIP=a
-	});
-}				
+async function ipAdresimiAl() {
+  await axios({
+    method: "get",
+    url: "https://apis.ergineer.com/ipadresim",
+  })
+    .then(function (response) {
+      return response.data;
+    })
+    .then(function (a) {
+      benimIP = a;
+    });
+}
 // ------------ değiştirmeyin --------------
-
 
 /*
 	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
@@ -67,6 +65,57 @@ async function ipAdresimiAl(){
 	Örnek dinamik URL kullanımı: var url = "https://apis.ergineer.com/ipgeoapi/"+benimIP; 
 */
 
-
-
 //kodlar buraya gelecek
+
+const ipKarti = veriler => {
+  const divCard = document.createElement("div");
+  divCard.classList.add("card");
+
+  const imgBayrak = document.createElement("img");
+  //imgBayrak.setAttribute("src", veriler.ülkebayrağı); data içerisindeki resim görüntülenmediği için ---> etütte yeni bayrak resmi ekledik.
+  //imgBayrak.src = "https://flagcdn.com" + "/256x192/" + veriler.ülkeKodu.toLowerCase() + ".png" ---> etütte eklenen bayrak
+  imgBayrak.src =
+    "https://www.geoguessr.com/seterra/images/system/flags/" +
+    veriler.ülke.toLowerCase() +
+    ".png";
+
+  const divİnfo = document.createElement("div");
+  divİnfo.classList.add("card-info");
+
+  const baslik = document.createElement("h3");
+  baslik.classList.add("ip");
+  baslik.textContent = veriler.sorgu;
+
+  const prg1 = document.createElement("p");
+  prg1.classList.add("ulke");
+  prg1.textContent = `${veriler.ülke} (${veriler.ülkeKodu})`;
+
+  const prg2 = document.createElement("p");
+  prg2.textContent = `Enlem: ${veriler.enlem} Boylam: ${veriler.boylam}`;
+
+  const prg3 = document.createElement("p");
+  prg3.textContent = `Şehir: ${veriler.şehir}`;
+
+  const prg4 = document.createElement("p");
+  prg4.textContent = `Saat Dilimi: ${veriler.saatdilimi}`;
+
+  const prg5 = document.createElement("p");
+  prg5.textContent = `Para Birimi: ${veriler.parabirimi}`;
+
+  const prg6 = document.createElement("p");
+  prg6.textContent = `ISP: ${veriler.isp}`;
+
+  divİnfo.append(baslik, prg1, prg2, prg3, prg4, prg5, prg6);
+
+  divCard.append(imgBayrak, divİnfo);
+
+  return divCard;
+};
+
+async function getData() {
+  await ipAdresimiAl();
+  axios.get("https://apis.ergineer.com/ipgeoapi/" + benimIP).then(response => {
+    document.querySelector(".cards").append(ipKarti(response.data));
+  });
+}
+getData();
